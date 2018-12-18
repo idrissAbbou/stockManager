f626a228ee0762188cacafa4e594d32d920fdf3c
idrissAbbou <idrisskapan53@gmail.com>
2018-12-18 13:55:10 +0000

    creating the Dal library

---
+++ Stock Manager/DAL/AdoDatabaseCreatorClasses/AdoDatabaseCreator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Configuration;
+
+namespace DAL.AdoDatabaseCreatorClasses
+{
+    /***********************************************************************
+     * kola object f database dir lih method dyalo bo7do 
+     * kola method dir task wa7d la ghayr
+     * fach tgadhom 3yt l kolchi f lconstructor bach tbuilda database
+     * dima lfunction trad chi haja li tbyn bila dakchi tkriya f ExecuteNonQuery
+       trj3 l effected lines o ila kan chi haja khas ikon fih exception trj3o
+     * ila khask SqlReader declarih f lclass machi f les method kola object 
+     * khaso ikon mara wa7da ila ila d3at darora ikon local
+    ************************************************************************/
+    public class AdoDatabaseCreator
+    {
+        private string _connectionString;
+        private SqlConnection _sqlConnection;
+        private SqlCommand _sqlCommand;
+
+        public AdoDatabaseCreator(sqlServerTypeEnum sqlServerType)
+        {
+            InitializeCompenent(sqlServerType);
+            Createdatabase();
+        }
+
+        private void InitializeCompenent(sqlServerTypeEnum sqlServerType)
+        {
+            SetConnectionString(sqlServerType);
+            _sqlConnection = new SqlConnection(_connectionString);
+            _sqlCommand = _sqlConnection.CreateCommand();
+        }
+
+        public void SetConnectionString(sqlServerTypeEnum sqlServerType)
+        {
+            switch (sqlServerType)
+            {
+                case sqlServerTypeEnum.sqlServer:
+                    _connectionString = ConfigurationManager.ConnectionStrings["sqlConnectionString"].ConnectionString;
+                    break;
+                case sqlServerTypeEnum.sqlServerExpress:
+                    _connectionString = ConfigurationManager.ConnectionStrings["sqlExpressConnectionString"].ConnectionString;
+                    break;
+                default:
+                    throw new ServerNotSupportedException();
+            }
+        }
+
+        public int Createdatabase()
+        {
+            int lineEffected = 0;
+            _sqlCommand.CommandText = "create database StockManager";
+            _sqlConnection.Open();
+            lineEffected = _sqlCommand.ExecuteNonQuery();
+            _sqlConnection.Close();
+            return lineEffected;
+        }
+
+        
+    }
+}
---
+++ Stock Manager/DAL/SmoDatabaseCreatorClasses/SmoDatabaseCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo.SqlEnum;
+
+namespace DAL.SmoDatabaseCreatorClasses
+{
+    public class SmoDatabaseCreator
+    {
+        ConnectionSettings _connectionSetting;
+        Server _server;
+        Database _database;
+
+        public SmoDatabaseCreator(sqlServerTypeEnum sqlServerType)
+        {
+            SetServer(sqlServerType);
+        }
+
+        public void SetServer(sqlServerTypeEnum sqlServerType)
+        {
+            switch (sqlServerType)
+            {
+                case sqlServerTypeEnum.sqlServer:
+                    _server = new Server(@".");
+                    break;
+                case sqlServerTypeEnum.sqlServerExpress:
+                    _server = new Server(@".\SQLEXPRESS");
+                    break;
+                default:
+                    throw new ServerNotSupportedException();
+            }
+        }
+
+        public void CreateDatabase()
+        {
+            _database = new Database(_server, "StockManager");
+            _database.Create();
+        }
+
+        public void CreateTableProduct()
+        {
+            Table product = new Table(_database, "Product");
+
+
+            Index index = new Index(product, "barecode_Index");
+            index.IsClustered = true;
+            IndexedColumn columnIndex = new IndexedColumn(index, "barecode", true);
+            index.IndexedColumns.Add(columnIndex);
+
+
+            Column column = new Column(product, "nameProduct", DataType.VarChar(50));
+            product.Columns.Add(column);
+            column = new Column(product, "priceProduct", DataType.Float);
+            product.Columns.Add(column);
+            column = new Column(product, "descriptionProduct", DataType.VarCharMax);
+            product.Columns.Add(column);
+            column = new Column(product, "imageProduct", DataType.VarBinaryMax);
+            product.Columns.Add(column);
+            column = new Column(product, "qteEnStockProduct", DataType.Int);
+            product.Columns.Add(column);
+
+            product.Create();
+
+        }
+
+    }
+}
